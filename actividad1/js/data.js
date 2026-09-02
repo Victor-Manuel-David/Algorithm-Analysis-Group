53faeabdf0563f84b29be6f119648a36f9fd2185
Sebastian Restrepo Mira <sebarestrepo97@hotmail.com>
2026-09-01 22:33:09 -0500

    feat(Actividad-01): implementar modelo de datos de deposito y entregas

--- actividad1/js/data.js
+++ actividad1/js/data.js
@@ -5,3 +5,24 @@
     SAMPLE_DELIVERIES: [],
   };
 })(window);
+
+(function loadSampleData(global) {
+  'use strict';
+
+  const DEPOT = Object.freeze({
+    id: 'depot',
+    name: 'Centro logístico',
+    x: 12,
+    y: 52,
+  });
+
+  const SAMPLE_DELIVERIES = Object.freeze([
+    Object.freeze({ id: 'a', name: 'Librería Central', x: 30, y: 27 }),
+    Object.freeze({ id: 'b', name: 'Café Aurora', x: 49, y: 70 }),
+    Object.freeze({ id: 'c', name: 'Clínica Norte', x: 76, y: 22 }),
+    Object.freeze({ id: 'd', name: 'Mercado Verde', x: 87, y: 63 }),
+    Object.freeze({ id: 'e', name: 'Edificio Luna', x: 60, y: 46 }),
+  ]);
+
+  global.RutaExpressData = Object.freeze({ DEPOT, SAMPLE_DELIVERIES });
+})(window);
